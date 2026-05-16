63d0def84140574d23eba49a11c0a712683288e2
alejandro montecinos <alejandromontecino002@gmail.com>
2026-05-15 22:48:51 -0400

    se arreglan los nombres de los archivos

--- front_despacho/src/componentes/Layouts/Reviews.jsx
+++ front_despacho/src/componentes/Layouts/Reviews.jsx
@@ -8,7 +8,7 @@ function Reviews() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 mx-auto"
-            src="/apple.pngg"
+            src="/apple.png"
             alt="Transistor"
             width="158"
             height="48"
